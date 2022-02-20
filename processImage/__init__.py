e5c4a42696fa3bdb7e7ff4d66cb44f867c6b08b1
Wei-Che Chung <wchung@seattleu.edu>
2022-02-19 23:16:40 -0800

    Handle error exceptions in init.py

--- processImage/__init__.py
+++ processImage/__init__.py
@@ -7,8 +7,6 @@ import urllib.request
 
 
 def flip(img, dir): # Vertically = 0, Horizontally = 1
-    if not (dir==0 or dir==1):
-        return False
     return cv2.flip(img, dir)
 
 
@@ -24,8 +22,6 @@ def convert_to_grayscale(img):
 
 
 def resize(img, percent): # percent = 1~1000
-    if(percent <= 0 or percent > 1000):
-        return False
     width = int(img.shape[1] * percent / 100)
     height = int(img.shape[0] * percent / 100)
     dim = (width, height)
@@ -51,38 +47,70 @@ def img_processor(img_url, ops_str):
     ops = []
     ops_str = ops_str.lower()
     ops_str = ops_str.split(' ')
-    url_response = urllib.request.urlopen(img_url)
-    img = cv2.imdecode(np.array(bytearray(url_response.read()), dtype=np.uint8), -1)
 
+    try:
+        url_response = urllib.request.urlopen(img_url)
+    except Exception as e:
+        return "Url open failed:" + str(e)
+    
+    try:
+        img = cv2.imdecode(np.array(bytearray(url_response.read()), dtype=np.uint8), -1)
+    except Exception as e:
+        return "Image decode failed:" + str(e)
+    
     for i in ops_str:
         i = i.strip().split(',')
         ops.append(i)
 
     for op in ops:
-        if(op[0] == 'flip'):
-            img = flip(img, int(op[1]))
-        if(op[0] == 'rotate'):
-            img = rotate(img, int(op[1]))
-        if(op[0] == 'grayscale'):
+        if op[0] == 'flip':
+            try:
+                dir = int(op[1])
+            except:
+                return 'The flip operation requires a parameter.'
+
+            if dir==0 or dir==1:
+                img = flip(img, dir)
+            else:
+                return 'The flip operation only accepts 0 or 1 as a parameter.'
+
+        elif op[0] == 'rotate':
+            try:
+                degree = int(op[1])
+            except:
+                return 'The rotate operation requires a parameter.'
+
+            img = rotate(img, degree)
+        elif op[0] == 'grayscale':
             img = convert_to_grayscale(img)
-        if(op[0] == 'resize'):
-            img = resize(img, int(op[1]))
-        if(op[0] == 'thumbnail'):
+        elif op[0] == 'resize':
+            try:
+                percentage = int(op[1])
+            except:
+                return 'The resize operation requires a parameter.'
+
+            if percentage>0 and percentage<=1000:
+                img = resize(img, percentage)
+            else:
+                return 'The resize operation only accepts 1 to 1000 as a parameter.'
+
+        elif op[0] == 'thumbnail':
             img = generate_thumbnail(img)
-        if(op[0] == 'rotatel'):
+        elif op[0] == 'rotatel':
             img = rotate_left(img)
-        if(op[0] == 'rotater'):
+        elif op[0] == 'rotater':
             img = rotate_right(img)
+        else:
+            return op[0] + ' is invalid operation.'
 
     return img
+
 
 def numpy_to_binary(img_ext, img_arr):
     is_success, buffer = cv2.imencode(img_ext, img_arr)
     io_buf = io.BytesIO(buffer)
     return io_buf.read()
 
-def raise_exception_msg(msg, status_code=400):
-    return func.HttpResponse(msg, status_code)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -90,21 +118,24 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     img = req.params.get('img')
     ops = req.params.get('ops')
     
-    if not (img and ops):
-        return raise_exception_msg("image url and operation are required")
-
-    img_ext = img[img.rfind('.')+1:]
-    mimetype = "image/" + img_ext
+    if img==None or ops==None:
+        return func.HttpResponse("Image url and operation are required.")
 
     try:
-        if img and ops:
-            img = img_processor(img, ops)
-            binary_img = numpy_to_binary('.' + img_ext, img)
-            return func.HttpResponse(binary_img, mimetype = mimetype)
-        else:
-            return func.HttpResponse(
-                "This HTTP triggered function executed successfully. Pass img and ops in the query string or in the request body for a personalized response.",
-                status_code=200
-            )
+        img_ext = img[img.rfind('.')+1:]
+        mimetype = "image/" + img_ext
     except:
-        return func.HttpResponse("Please check the parameters. The image url must contain the file type. The flip method only accepts 0 and 1. The resize method only accepts 1 to 1000")
+        return func.HttpResponse("Image url invalid.")
+
+    try:
+        img = img_processor(img, ops)
+
+        try:
+            binary_img = numpy_to_binary('.' + img_ext, img)
+        except:
+            return func.HttpResponse(img, status_code=400)
+
+        return func.HttpResponse(binary_img, mimetype = mimetype)
+
+    except Exception as e:
+        return func.HttpResponse(str(e), status_code=400)
