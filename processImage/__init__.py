6116a3fd5124db29fe3f1ba21269defe44874d85
ChungNYCU <79520786+ChungNYCU@users.noreply.github.com>
2022-02-25 00:00:03 -0800

    Migrate GET requst to POST requst in init.py

--- processImage/__init__.py
+++ processImage/__init__.py
@@ -2,24 +2,37 @@ import azure.functions as func
 import logging
 from processImage import processor
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    img = req.params.get('img').strip()
-    ops = req.params.get('ops').strip()
-    
-    if img==None or ops==None:
-        return func.HttpResponse("Image url and operation are required.", status_code=400)
+    if req.method != 'POST':
+        return func.HttpResponse("Use Http Post", status_code=405)
 
     try:
-        img_ext = img[img.rfind('.')+1:]
+        ops = req.form.get('ops').strip()
+    except Exception as e:
+        return func.HttpResponse('Process operations failed, exception= [%s]' % e, status_code=400)
+
+    try:
+        uploadFile = req.files.get('img')
+        filename = uploadFile.filename
+        img = uploadFile.read()
+    except Exception as e:
+        return func.HttpResponse('Process file failed, exception= [%s]' % e, status_code=400)
+
+    if img == None or ops == None:
+        return func.HttpResponse("Image and operation are required.", status_code=400)
+
+    try:
+        img_ext = filename[filename.rfind('.')+1:]
         if(img_ext == 'tif'):
             img_ext = 'tiff'
             mimetype = "image/" + img_ext
         else:
             mimetype = "image/" + img_ext
     except:
-        return func.HttpResponse("Image url invalid.", status_code=400)
+        return func.HttpResponse("File extension invalid.", status_code=400)
 
     try:
         img = processor.img_processor(img, ops)
@@ -28,7 +41,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
         binary_img = processor.numpy_to_binary('.' + img_ext, img)
-    except:
-        return func.HttpResponse(img, status_code=400)
+    except Exception as e:
+        return func.HttpResponse("Cannot convert numpy array to image. " + str(e), status_code=400)
 
-    return func.HttpResponse(binary_img, mimetype = mimetype, status_code=200)
+    return func.HttpResponse(binary_img, mimetype=mimetype, status_code=200)
